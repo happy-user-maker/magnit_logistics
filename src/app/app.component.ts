7edc00226c6839ccc72031c903ca04313eb18bc8
Maxim Aleshin <maxim.aleshin@slcode.com>
2022-07-06 18:49:57 +0300

    first angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'magnit_logistics';
+  options = {
+    // layers: [
+    //   tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    // ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
 }
